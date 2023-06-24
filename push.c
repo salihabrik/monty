aeb096e71b852d5127b48ef233adfb1288e9f77e
root <root@DESKTOP-0ASGO79.localdomain>
2023-06-24 19:32:05 +0200

    Positive anything is better than negative nothing

--- push.c
+++ push.c
@@ -1,6 +1,29 @@
-#include <stdio.h>
-#include <stdlib.h>
+
 #include "monty.h"
+int value;
+
+/**
+ * createNode - creates a new node
+ * @n: the value of the node
+ * Return: new node
+ */
+
+stack_t *createNode(int n)
+{
+	stack_t *new = NULL;
+
+	new = malloc(sizeof(stack_t));
+	if (new == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+	new->n = n;
+	new->next = NULL;
+	new->prev = NULL;
+
+	return (new);
+}
 
 /**
  * push - Pushes an element to the stack
