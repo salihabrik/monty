0a2d65aa7ae10059a890f8c6dc7f812344353f32
salihabrik <briksaliha@gmail.com>
2023-06-24 18:22:06 -0700

    h

--- monty.c
+++ monty.c
@@ -1,45 +1,41 @@
 #include "monty.h"
-#define MAX_LINE_LENGTH 256
-/**
-* main - monty code interpreter
-* @argc: number of arguments
-* @argv: monty file location
-* Return: 0 on success
-*/
-int  main(int argc, char *argv[])
-{
-	char *content;
-	FILE *file;
-	size_t size = 0;
-	ssize_t read_line = 1;
-	stack_t *stack = NULL;
-	unsigned int counter = 0;
+#include <stdio.h>
+#include <stdlib.h>
 
-	if (argc != 2)
-	{
-		fprintf(stderr, "USAGE: monty file\n");
-		exit(EXIT_FAILURE);
-	}
-	file = fopen(argv[1], "r");
-	bus.file = file;
-	if (!file)
-	{
-		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-		exit(EXIT_FAILURE);
-	}
-	while (read_line > 0)
-	{
-		content = NULL;
-		read_line = getline (&content, &size, file);
-		bus.content = content;
-		counter++;
-		if (read_line > 0)
-		{
-			execute(content, &stack, counter, file);
-		}
-		free(content);
-	}
-	free_stack(stack);
-	fclose(file);
-return (0);
+#define MAX_LINE_LENGTH 256
+
+int main(int argc, char *argv[])
+{
+    char line[MAX_LINE_LENGTH];
+    FILE *file;
+    stack_t *stack = NULL;
+    unsigned int counter = 0;
+
+    if (argc != 2)
+    {
+        fprintf(stderr, "USAGE: monty file\n");
+        exit(EXIT_FAILURE);
+    }
+
+    file = fopen(argv[1], "r");
+    bus.file = file;
+
+    if (!file)
+    {
+        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+        exit(EXIT_FAILURE);
+    }
+
+    while (fgets(line, sizeof(line), file))
+    {
+        counter++;
+        bus.content = line;
+        execute(line, &stack, counter, file);
+    }
+
+    free_stack(stack);
+    fclose(file);
+
+    return 0;
 }
+
--- stack.c
+++ stack.c
@@ -11,3 +11,22 @@ void fstack(stack_t **head, unsigned int counter)
 	(void)counter;
 	bus.lifi = 0;
 }
+
+
+#include "monty.h"
+/**
+* free_stack - frees a doubly linked list
+* @head: head of the stack
+*/
+void free_stack(stack_t *head)
+{
+	stack_t *aux;
+
+	aux = head;
+	while (head)
+	{
+		aux = head->next;
+		free(head);
+		head = aux;
+	}
+}
